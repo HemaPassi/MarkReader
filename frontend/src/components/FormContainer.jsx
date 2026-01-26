cb1c3fd2c27c5185433a283cf46e51a36eb6460f
Hema Passi <hema.passi@gmail.com>
2026-01-27 00:27:21 +0530

    signup form

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,10 +1,11 @@
 import { Col, Container, Row } from "react-bootstrap";
-const FormContainer = ({ Children }) => {
+const FormContainer = ({ children }) => {
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col xs={12} md={6}></Col>
-        {Children}
+        <h5>In Form component</h5>
+        {children}
       </Row>
     </Container>
   );
