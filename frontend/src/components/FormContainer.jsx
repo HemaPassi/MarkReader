03e6e52da8daf2bdc8d72476e65a49639f4163b4
Hema Passi <hema.passi@gmail.com>
2026-01-27 22:45:10 +0530

    login and form related fixes

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -4,7 +4,6 @@ const FormContainer = ({ children }) => {
     <Container>
       <Row className="justify-content-md-center">
         <Col xs={12} md={6}></Col>
-        <h5>In Form component</h5>
         {children}
       </Row>
     </Container>
